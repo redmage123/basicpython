37a67243ba2b35426b468214ed9daac533147061
bbrelin <bbrelin@gmail.com>
2016-12-24 12:35:28 +0200

    Added lab3

---
+++ labs/lab3/lab3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import math
+cartesian = [(1,3),(4,4),(2,6),(-5,2),(2,1),(1,3),(2,6)]
+
+# Remove duplicates from the cartesian list.
+
+
+# Calculate the polar coordinates from the given cartesian coordinate list.
+
+# Create a list of the first 10,000 integers and calculate which of them are perfect numbers.
+# possibleperfects = 
+
+
+
